1f1c5eaab3a12bea2168ae7d3d5ad711fbafe6a5
Uriens <ignaciocstlls@gmail.com>
2023-02-23 17:10:45 +0100

    carousel

---
+++ prueba/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Carousel from './componentes/carousel'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div><Carousel /></div>
+    </div>
+  );
+}
+
+export default App;
