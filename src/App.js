34e666fcd3b6d402e34974c7e1df908d21f3331c
ManuKovaSG <kovacicmanu@gmail.com>
2023-02-23 08:55:07 +0100

    estructura y enrutamiento

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Components/Layout';
+import Landing from './Pages/landing';
+import Main from './Pages/main';
+import ProductPage from './Pages/product-page';
 
-function App() {
+
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+        <Route element={<Layout />} />
+        <Route index element={<Landing/>} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/product-page" element={<ProductPage />} />
+    </Routes>
+</>
   );
 }
 
