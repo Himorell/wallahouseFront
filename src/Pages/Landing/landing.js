a01dd75f5fb60615e4945fcd91034363864d3522
adria15gomez <adria15gomez@gmail.com>
2023-02-23 15:43:17 +0100

    style landing

--- src/Pages/Landing/landing.js
+++ src/Pages/Landing/landing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '../Landing/landing.css';
+import "../Landing/landing.css";
 import quiero from "../../Assets/Landing/quierounacasa.svg";
 import tengo from "../../Assets/Landing/tengounacasa.svg";
 import logo from "../../Assets/Landing/wallahouselogo.svg";
@@ -9,43 +9,38 @@ import { Container } from "semantic-ui-react";
 
 const Landing = () => {
   return (
-    <>
-    <Container>
-      <div className="container">
-        <div className="landingOnelogo">
-          <img src={logo} alt="logo"></img>
-        </div>
+    <div>
+      <Container>
+        <div className="container">
+          <div className="landingOnelogo">
+            <img src={logo} alt="logo"></img>
+          </div>
 
-        <div className="house">
-          <div className="containerImage">
-            <img src={hero} alt="hero"></img>
-            <h1>INTERCAMBIO DE</h1>
-            <h1>CASAS VACACIONALES</h1>
+          <div className="house">
+            <div className="containerImage">
+              <img src={hero} alt="hero" className="imgHouse"></img>
+              <div className="titleContainerHome">
+                <h1>INTERCAMBIO DE</h1>
+                <h1>CASAS VACACIONALES</h1>
+              </div>
+            </div>
+
+            <div className="containerButton">
+              <a href="/profile">
+                {" "}
+                <img src={tengo} alt="tengo"></img>
+              </a>
+              <a href="/main">
+                {" "}
+                <img src={quiero} alt="quiero"></img>
+              </a>
+            </div>
           </div>
-          
-     
-          <div className="containerButton">
-          <a href="/profile"> <img src={tengo} alt='tengo'></img></a>
-          <a href="/main"> <img src={quiero} alt='quiero'></img></a>
-          </div>
- 
         </div>
-      </div>
       </Container>
       <Footer />
-    </>
+    </div>
   );
 };
 
 export default Landing;
-
-          {/*   <ButtonHome
-              className="btnOne"
-              action="/howToProtect"
-              text="TENGO UNA CASA!"
-            /> */}
-            {/* <ButtonHome
-              className="btnTwo"
-              action="/howToProtect"
-              text="QUIERO UNA CASA!"
-            /> */}
